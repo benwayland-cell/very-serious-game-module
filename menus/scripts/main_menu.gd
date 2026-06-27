38651f547b196861af4f68a5a8f115008bc1dd6c
benwayland-cell <ben.wayland2007@gmail.com>
2026-06-26 18:18:50 -0600

    Updated buttons more

--- menus/scripts/main_menu.gd
+++ menus/scripts/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	if SceneManager.last_unlocked_level > 0:
+		%"New-ContinueButton".text = "Continue"
 
 func _on_new_continue_button_pressed() -> void:
 	SceneManager.load_last_unlocked_level()
@@ -11,3 +14,4 @@ func _on_level_select_button_pressed() -> void:
 
 func _on_custom_button_pressed() -> void:
 	SceneManager.last_unlocked_level = 1
+	%"New-ContinueButton".text = "New Game"
